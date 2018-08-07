51ecd0419bd1d62e11976d4d696d113ce8189a6e
YishengjieQAQ <547589605@qq.com>
2018-08-07 14:52:46 +0800

    Revert "Initial commit"
    This reverts commit 93ff6e35dd58e3b8eda5e9a5e54e8cbb3b084f64.

--- app/function_class/search.py
+++
@@ -1,7 +0,0 @@
-def isbn_or_key(word):
-    isbn_or_key = 'key'
-    if len(word) == 13 and word.isdigit():
-        return 'isbn'
-    shor_word = word.replace('-', '')
-    if len(word) == 10 and shor_word.isdigit():
-        return 'isbn'
